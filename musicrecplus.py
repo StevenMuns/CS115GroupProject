5cfb724526426e7e1087c6acc7845e95ed95cb08
StevenMuns <119441165+StevenMuns@users.noreply.github.com>
2022-11-30 18:12:39 -0500

    Add files via upload

--- musicrecplus.py
+++ musicrecplus.py
@@ -39,7 +39,71 @@ def e():
     f.pop(name_artists)
     a = {name_artists: []}
     f.update(appArtists(a, name_artists))
-    ''' Returns a dictionary sorted by lowest frequently occuring artists to highest frequently returning artists
+    choices()
+
+def r():
+    """ prints recommended artists to the user.
+        if no recommendations are found, prints "no preferences available at this time"
+        Emma Millet
+    """
+    dictionary = globalDict
+    username = globalUsername
+
+    if len(dictionary.keys()) == 1:
+        return print("No preferences available at this time.")
+    prefs = dictionary.get(username)
+   
+    best = -1
+    prefs = dictionary.get(username)
+
+    bestUser = ""
+    for user in dictionary.keys():
+        if user[-1] == "$":
+            continue
+        else:
+            score = numMatches(prefs, dictionary[user])
+            if score > best and username != user:
+                best = score
+                bestUser = user
+   
+    rec = []
+    for item in dictionary.get(bestUser):
+        if ((item in prefs) != True):
+            rec += [item]
+   
+    if (rec == []):
+        return print("No preferences available at this time.")
+    for artist in rec:
+        print(artist)
+
+def edit(list1, list2):
+    """returns a new list that contains only the elements in list2 not in list1
+        Emma Millet
+    """
+    newL = []
+    for item in list2:
+        if ((item in list1) != True):
+            newL += [item]
+    return newL
+
+def numMatches(list1, list2):
+    """ return the num of elements that match between two sorted lists
+         taken from textbook ; Emma Millet
+    """
+    matches, i, j = 0, 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] == list2[j]:
+            matches += 1
+            i += 1
+            j += 1
+        elif list1[i] < list2[j]:
+            i += 1
+        else:
+            j += 1
+    return matches
+
+def popDictMaker():
+    ''' Returns a dictionary sorted by lowest frequently occuring artists to highest frequently returning artists 
     Steven Munson, edited by Emma Millet'''
     a = []
     for key in globalDict:
@@ -66,6 +130,41 @@ def e():
 
 def p():
     '''Shows the top 3 most popular artists by how frequently each show up in each users preferences; "Sorry, no user found." otherwise
+    Steven Munson'''
+    c = popDictMaker()
+    if len(c) == 0:
+        print("Sorry , no artists found.")
+        return choices()
+
+    elif len(c) < 3:
+        w = list(c.keys())
+        for x in range(len(c)):
+            print(w[x])
+        return choices()
+    else:
+        w = list(c.keys())
+        for x in range(3):
+            print(w[-1])
+            w = w[:-1]
+        return choices()
+
+def h():
+    '''Prints number of likes the most popular artist recieved. If there are no artists found, prints "Sorry, no artists found."
+    Emma Millet'''
+    c = popDictMaker()
+    biggest = -1
+
+    if c != {}:
+        for artist in c:
+            if c[artist] >= biggest:
+                biggest = c[artist]
+        print(biggest)
+    else:
+        print("Sorry, no artists found.")
+    return choices()
+
+def m():
+    '''
        Prints the full name of the user who likes the most artists; "Sorry, no user found." otherwise
        Steven Munson
     '''
@@ -97,7 +196,7 @@ def choices():
         else:
             choice = input("Enter a letter to choose an option:\ne - Enter preferences\nr - Get recommendations\np - Show most popular artists\nh - How popular is the most popular\nm - Which user has the most likes\nq - Save and quit\n")
 
-    with open('musicrecplus.txt', "r+") as file:
+with open('musicrecplus.txt', "r+") as file:
         for user in globalDict:
             file.truncate()
             file.seek(0)
